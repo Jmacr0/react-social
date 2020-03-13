89c3908c0ffd3dd36e3f1fbb5a06acfcc9ef1162
Jmacro <jonmacrohon@gmail.com>
2020-03-13 22:39:13 +1100

    button component performs as a navlink in the sidebar

---
+++ src/components/SideBar/Button.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const LinkStyle = styled.div`
+	.link {
+		padding: 6px 8px 6px 16px;
+		text-decoration: none;
+		font-size: 25px;
+		display: block;					
+		cursor: pointer;
+		color: white;
+		transition-duration: 0.4s;
+		white-space: nowrap;
+		
+		&:hover {
+			background-color: #333;
+			color: red;
+		}
+	}
+`
+
+export const Button = ({ value, link }) => {
+	return (
+		<>
+			<LinkStyle>
+				<Link to={link} replace className='link'>
+					{value}
+				</Link>
+			</LinkStyle>
+		</>
+	)
+}
