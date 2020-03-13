6c4e7ea9c44f83b8cc738ad9e1a5f24c6ba7160e
Jmacro <jonmacrohon@gmail.com>
2020-03-13 22:47:06 +1100

    Toggle component that opens and closes the sidebar

---
+++ src/components/SideBar/Toggle.js
@@ -0,0 +1,47 @@
+import React, { useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+import { ToggleContext } from '../../utils/ToggleContext';
+
+const IconStyle = styled.div`
+	&& {
+		color: white;
+		font-size: 30px;
+		margin-right: 10px;
+		width: 200px;		
+	}	
+
+	.cursor {
+		cursor: pointer;
+	}
+
+	.arrow {
+		color: grey;
+		animation-name: tap;
+		animation-duration: 0.5s;
+		animation-direction: alternate;
+		animation-iteration-count: infinite;
+		animation-timing-function: cubic-bezier(1, 1, 1, 0.2);
+	}
+
+	@keyframes tap {
+		from {margin-left: 0px;}
+		to {margin-left: 10px;}
+	}
+`
+
+export const Toggle = () => {
+	const toggle = useContext(ToggleContext);
+	const arrow = toggle.collapse ? '' : <FontAwesomeIcon icon={faLongArrowAltLeft} className='arrow' />;
+
+	return (
+		<IconStyle>
+			<FontAwesomeIcon onClick={() => toggle.onClick(toggle.collapse ? false : true)} icon={faAddressCard} className='cursor ml-2' />
+			{arrow}
+		</IconStyle>
+	);
+}
+
+
