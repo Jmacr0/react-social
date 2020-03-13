7a6340731d50dbe734cad8a566c47ff391a9b64d
Jmacro <jonmacrohon@gmail.com>
2020-03-13 22:19:40 +1100

    Search bar component in the nav

---
+++ src/components/Search/Search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Search = () => {
+	return (
+		<div>
+			<input placeholder='Search' className='rounded p-1 my-1' style={{ border: 'none' }} />
+		</div>
+	)
+}
