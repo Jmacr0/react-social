e342b46d89c9fa60f3b83caf93fe53cef38f4654
Jmacro <jonmacrohon@gmail.com>
2020-03-16 00:04:35 +1100

    added category drop down to creat review

--- src/components/CreateReview/CreateReview.js
+++ src/components/CreateReview/CreateReview.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Form, Container, Button, Row, Col } from 'react-bootstrap';
+import { Form, Container, Button, Row, Col, Dropdown } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { ToggleContext } from '../../utils/ToggleContext';
@@ -19,6 +19,10 @@ export const CreateReview = () => {
 	const [item, setItem] = useState('');
 	const [title, setTitle] = useState('');
 	const [rating, setRating] = useState('');
+	const [categoryType, setCategoryType] = useState({
+		category: 'Miscellaneous',
+		colour: 'secondary'
+	});
 	const [pros, setPros] = useState('');
 	const [cons, setCons] = useState('');
 	const [description, setDescription] = useState('');
@@ -29,10 +33,12 @@ export const CreateReview = () => {
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
+		const { category } = categoryType;
 		const newReview = {
 			item,
 			title,
 			rating,
+			category,
 			pros,
 			cons,
 			description
@@ -50,37 +56,69 @@ export const CreateReview = () => {
 					<Form onSubmit={handleSubmit}>
 						<Form.Group controlId="exampleForm.ControlInput1">
 							<Form.Label>Item *</Form.Label>
-							<Form.Control type="text" name='item' value={item} placeholder="'Airpods'" onChange={e => setItem(e.target.value)} />
+							<Form.Control type="text" name='item' value={item} placeholder="Airpods" onChange={e => setItem((e.target.value).substring(0, 30))} />
 						</Form.Group>
 						<Form.Group controlId="exampleForm.ControlInput2">
 							<Form.Label>Title *</Form.Label>
-							<Form.Control type="text" name='title' value={title} placeholder="'Well, that was quite expensive!'" onChange={e => setTitle(e.target.value)} />
+							<Form.Control type="text" name='title' value={title} placeholder="Well, that was quite expensive!" onChange={e => setTitle((e.target.value).substring(0, 30))} />
 						</Form.Group>
 						<Row>
 							<Col>
 								<Form.Group controlId="exampleForm.ControlInput3">
 									<Form.Label style={{ color: 'green', fontWeight: 'bold' }}>Pros</Form.Label>
-									<Form.Control type="text" name='title' value={pros} placeholder="'Compatible with all my apple products ( the ecosystem ! )'" onChange={e => setPros(e.target.value)} />
+									<Form.Control type="text" name='title' value={pros} placeholder="Compatible with all my apple products ( the ecosystem ! )" onChange={e => setPros(e.target.value)} />
 								</Form.Group>
 							</Col>
 							<Col>
 								<Form.Group controlId="exampleForm.ControlInput4">
 									<Form.Label style={{ color: 'red', fontWeight: 'bold' }}>Cons</Form.Label>
-									<Form.Control type="text" name='title' value={cons} placeholder="'Expensive'" onChange={e => setCons(e.target.value)} />
+									<Form.Control type="text" name='title' value={cons} placeholder="Expensive" onChange={e => setCons(e.target.value)} />
 								</Form.Group>
 							</Col>
 						</Row>
-						<Form.Group>
-							<Form.Label>Rating <span role='img' aria-label='star-emoji'>⭐</span> *</Form.Label>
-							<div key='inline-radio' className="mb-3">
-								{[1, 2, 3, 4, 5].map((rate, index) => {
-									return <Form.Check inline key={index} name='ratingSelection' label={rate} type='radio' id={`inline-radio-${rate}`} onChange={() => setRating(rate)} checked={rating === rate} />
-								})}
-							</div>
-						</Form.Group>
+						<Row>
+							<Col>
+								<Form.Group>
+									<Form.Label>Rating <span role='img' aria-label='star-emoji'>⭐</span> *</Form.Label>
+									<div key='inline-radio' className="mb-3">
+										{[1, 2, 3, 4, 5].map((rate, index) => {
+											return <Form.Check inline key={index} name='ratingSelection' label={rate} type='radio' id={`inline-radio-${rate}`} onChange={() => setRating(rate)} checked={rating === rate} />
+										})}
+									</div>
+								</Form.Group>
+							</Col>
+							<Col>
+								<Form.Group>
+									<Form.Label>Category</Form.Label>
+									<Dropdown>
+										<Dropdown.Toggle variant={categoryType.colour}>
+											{categoryType.category}
+										</Dropdown.Toggle>
+										<Dropdown.Menu>
+											<Dropdown.Item
+												onClick={() => setCategoryType({
+													category: 'Technology',
+													colour: 'warning'
+												})
+												}>Technology</Dropdown.Item>
+											<Dropdown.Item onClick={() => setCategoryType({
+												category: 'Fashion',
+												colour: 'success'
+											})
+											}>Fashion</Dropdown.Item>
+											<Dropdown.Item onClick={() => setCategoryType({
+												category: 'Miscellaneous',
+												colour: 'secondary'
+											})
+											}>Miscellaneous</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Form.Group>
+							</Col>
+						</Row>
 						<Form.Group controlId="exampleForm.ControlTextarea1">
 							<Form.Label>Your Review *</Form.Label>
-							<Form.Control as="textarea" rows="4" placeholder="'Great sound but doesn't offer the best value propostion. Other similar quality earphones for much cheaper.'" value={description} onChange={e => setDescription(e.target.value)} />
+							<Form.Control as="textarea" rows="4" placeholder="Great sound but doesn't offer the best value propostion. Other similar quality earphones for much cheaper." value={description} onChange={e => setDescription(e.target.value)} />
 						</Form.Group>
 						<Button type='submit'>Save</Button>
 					</Form>
