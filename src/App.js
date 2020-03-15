993be4b50c4a6318352ca861ec89ca054f4b05bc
Jmacro <jonmacrohon@gmail.com>
2020-03-15 19:21:17 +1100

    added authenticate state

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { Home } from './components/Home';
+import { SignUp } from './components/SignUp';
 import { NoMatch } from './components/NoMatch';
+import API from './utils/API';
 
 function App() {
+	const [authenticated, setAuthenticated] = useState({
+		authenticated: false,
+	});
+
+	const authenticate = async () => {
+		const isAuthenticated = await API.authenticateUser();
+		setAuthenticated(isAuthenticated);
+	}
+
+	useEffect(() => {
+		authenticate();
+	}, [])
+
 	return (
-		<React.Fragment>
+		<>
 			<Router>
 				<>
 					<Switch>
-						<Route path='/' component={Home} />
+						<Route
+							exact path='/sign-up'
+							render={() => <SignUp onAuthenticate={setAuthenticated} />}
+						/>
+						{authenticated ? <Route path='/' component={Home} /> : <Redirect to='/sign-up' />}
 						<Route path='*' component={NoMatch} />
 					</Switch>
 				</>
 			</Router>
-		</React.Fragment>
+		</>
 	);
 }
 
