2d8cbb3f28374f0d56fff0422f58954137226bf5
Jmacro <jonmacrohon@gmail.com>
2020-03-15 19:25:56 +1100

    added API functions: saveReview, saveUser, loginUser, logoutUser, authenticateUser

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default {
 	// Gets all books
 	getReviews: async function () {
-		const response = await axios.get("/api/reviews");
+		const response = await axios.get("/api/review/all");
 		return response.data;
 	},
 	// // Gets the book with the given id
@@ -14,8 +14,30 @@ export default {
 	// deleteBook: function (id) {
 	// 	return axios.delete("/api/books/" + id);
 	// },
-	// // Saves a book to the database
-	// saveBook: function (bookData) {
-	// 	return axios.post("/api/books", bookData);
-	// }
+	// Saves a review to the database
+	saveReview: async function (newReview) {
+		const response = await axios.post("/api/review/new", newReview, { withCredentials: true });
+		console.log(response)
+		return response.data;
+	},
+	// Saves a new user to the database
+	saveUser: async function (newUser) {
+		const response = await axios.post("/api/user/new", newUser);
+		return response;
+	},
+	// Logs the user in
+	loginUser: async function (existingUser) {
+		const response = await axios.post("/api/user/login", existingUser);
+		return response.data;
+	},
+	logoutUser: async function () {
+		const response = await axios.get("/api/user/logout");
+		console.log(response.data)
+		return response.data;
+	},
+	authenticateUser: async function () {
+		const response = await axios.get('/api/user/authenticate')
+		console.log(response.data)
+		return response.data;
+	}
 };
