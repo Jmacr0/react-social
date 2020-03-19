5d766ca181fe49d53172d9593cc92d82c9a6e7f4
Jmacro <jonmacrohon@gmail.com>
2020-03-20 00:12:18 +1100

    added new routes: feed, category(home), profile edit, review one, review new

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,8 @@ import { Profile } from './Profile/Profile';
 import { Navigation } from './Navigation/Navigation';
 import { Review } from './Review/Review';
 import { CreateReview } from './CreateReview/CreateReview';
+import { ProfileEdit } from './Profile/ProfileEdit';
+import { Category } from './Category/Category';
 
 export const Home = () => {
 	const [toggleState, setToggleState] = useState({
@@ -20,10 +22,12 @@ export const Home = () => {
 		<ToggleContext.Provider value={toggleState}>
 			<Navigation />
 			<SideBar />
-			<Route exact path='/' component={Feed} />
-			<Route path='/profile' component={Profile} />
-			<Route exact path='/review' component={Review} />
-			<Route path='/review/new' component={CreateReview} />
+			<Route exact path='/profile/edit' component={ProfileEdit} />
+			<Route exact path='/profile' component={Profile} />
+			<Route exact path='/review/one/:id' component={Review} />
+			<Route exact path='/review/new' component={CreateReview} />
+			<Route exact path='/feed' component={Feed} />
+			<Route exact path='/' component={Category} />
 		</ToggleContext.Provider >
 	)
 }
