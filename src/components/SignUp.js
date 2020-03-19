c91ff8d7a619565a250b5bd1a10a24005b3b4edd
Jmacro <jonmacrohon@gmail.com>
2020-03-20 00:16:30 +1100

    created SignUp component which handles sign up and login input, sends to back end with authentication

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,7 +4,7 @@ import { Redirect } from 'react-router-dom';
 import API from '../utils/API';
 
 export const SignUp = ({ onAuthenticate }) => {
-	const [toggleLogin, setToggleLogin] = useState(false);
+	const [toggleLogin, setToggleLogin] = useState(true);
 	const [username, setUsername] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
@@ -40,7 +40,7 @@ export const SignUp = ({ onAuthenticate }) => {
 				password,
 				confirmPassword
 			}
-			API.saveUser(newUser)
+			API.user.saveUser(newUser)
 				.then(success => {
 					setAlert({
 						message: 'Successfully Signed Up!',
@@ -65,7 +65,7 @@ export const SignUp = ({ onAuthenticate }) => {
 				username,
 				password
 			}
-			API.loginUser(existingUser)
+			API.user.loginUser(existingUser)
 				.then(redirectRoute => {
 					onAuthenticate(true);
 					setRedirect(redirectRoute);
@@ -81,73 +81,77 @@ export const SignUp = ({ onAuthenticate }) => {
 
 	return (
 		<Container>
-			<Form onSubmit={handleSubmit} className='shadow'>
-				{alert.type ? (
-					<Row>
-						<Col>
-							<Alert variant={alert.type}>
-								{alert.message}
-							</Alert>
-						</Col>
-					</Row>
-				) : ''
-				}
-				<Row className='px-3 pb-3'>
-					<Col>
-						<Button
-							variant='info'
-							onClick={() => {
-								setToggleLogin(toggleLogin ? false : true)
-								setUsername('');
-								setEmail('');
-								setPassword('');
-								setConfirmPassword('');
-							}
-							}>
-							{toggleLogin ? 'New User? Signup here!' : 'Existing User? Login here!'}
-						</Button>
-						{toggleLogin ? (
-							<>
-								<h2>Login</h2>
-								<Form.Group controlId="formBasicUsername">
-									<Form.Label>Username</Form.Label>
-									<Form.Control type="text" value={username || ''} placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
-								</Form.Group>
-								<Form.Group controlId="formBasicPassword">
-									<Form.Label>Password</Form.Label>
-									<Form.Control type="password" value={password || ''} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-								</Form.Group>
-								<Form.Group controlId="formBasicCheckbox">
-									<Form.Check type="checkbox" label='Remember Me' />
-								</Form.Group>
-							</>
-						) : (
-								<>
-									<h2>Sign Up</h2>
-									<Form.Group controlId="formBasicUsername">
-										<Form.Label>Username</Form.Label>
-										<Form.Control type="text" value={username || ''} placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
-									</Form.Group>
-									<Form.Group controlId="formBasicEmail">
-										<Form.Label>Email address</Form.Label>
-										<Form.Control type="email" value={email || ''} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
-									</Form.Group>
-									<Form.Group controlId="formBasicPassword">
-										<Form.Label>Password</Form.Label>
-										<Form.Control type="password" value={password || ''} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-									</Form.Group>
-									<Form.Group controlId="formBasicConfirmPassword">
-										<Form.Label>Confirm Password</Form.Label>
-										<Form.Control type="password" value={confirmPassword || ''} placeholder="Password" onChange={(e) => setConfirmPassword(e.target.value)} />
-									</Form.Group>
-								</>
-							)}
-					</Col>
-				</Row>
-				<Button variant="primary btn-block rounded-0" type="submit">
-					Submit
+			<Row>
+				<Col md={{ span: 8, offset: 2 }}>
+					<Form onSubmit={handleSubmit} className='shadow'>
+						{alert.type ? (
+							<Row>
+								<Col>
+									<Alert variant={alert.type}>
+										{alert.message}
+									</Alert>
+								</Col>
+							</Row>
+						) : ''
+						}
+						<Row className='px-3 pb-3'>
+							<Col>
+								<Button
+									variant='info'
+									onClick={() => {
+										setToggleLogin(toggleLogin ? false : true)
+										setUsername('');
+										setEmail('');
+										setPassword('');
+										setConfirmPassword('');
+									}
+									}>
+									{toggleLogin ? 'New User? Signup here!' : 'Existing User? Login here!'}
+								</Button>
+								{toggleLogin ? (
+									<>
+										<h2>Login</h2>
+										<Form.Group controlId="formBasicUsername">
+											<Form.Label>Username</Form.Label>
+											<Form.Control type="text" value={username || ''} placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
+										</Form.Group>
+										<Form.Group controlId="formBasicPassword">
+											<Form.Label>Password</Form.Label>
+											<Form.Control type="password" value={password || ''} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+										</Form.Group>
+										<Form.Group controlId="formBasicCheckbox">
+											<Form.Check type="checkbox" label='Remember Me' />
+										</Form.Group>
+									</>
+								) : (
+										<>
+											<h2>Sign Up</h2>
+											<Form.Group controlId="formBasicUsername">
+												<Form.Label>Username</Form.Label>
+												<Form.Control type="text" value={username || ''} placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
+											</Form.Group>
+											<Form.Group controlId="formBasicEmail">
+												<Form.Label>Email address</Form.Label>
+												<Form.Control type="email" value={email || ''} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+											</Form.Group>
+											<Form.Group controlId="formBasicPassword">
+												<Form.Label>Password</Form.Label>
+												<Form.Control type="password" value={password || ''} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+											</Form.Group>
+											<Form.Group controlId="formBasicConfirmPassword">
+												<Form.Label>Confirm Password</Form.Label>
+												<Form.Control type="password" value={confirmPassword || ''} placeholder="Password" onChange={(e) => setConfirmPassword(e.target.value)} />
+											</Form.Group>
+										</>
+									)}
+							</Col>
+						</Row>
+						<Button variant="primary btn-block rounded-0" type="submit">
+							Submit
   				</Button>
-			</Form>
+					</Form>
+				</Col>
+			</Row>
 			{redirect ? <Redirect to={redirect} /> : ''}
 		</Container >
 	)
