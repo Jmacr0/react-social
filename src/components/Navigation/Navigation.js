1b298904e21bbbb71505e607322dc8f77879a059
Jmacro <jonmacrohon@gmail.com>
2020-03-20 00:32:25 +1100

    conditionally render search bar depending on route. if category, dont render in nav

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { Search } from '../Search/Search';
 import { ToggleContext } from '../../utils/ToggleContext';
-import { Link } from 'react-router-dom';
 
 const NavStyle = styled.div`
 	background-image: linear-gradient(#0271f1, #00c3ff);
@@ -13,11 +13,13 @@ const NavStyle = styled.div`
 	.openNav {
 		margin-left: 220px;
 		padding: 0px 10px;
+		transition-duration: 0.2s;
 	}
 
 	.closedNav {
 		margin-left: 50px;
 		padding: 0px 10px;
+		transition-duration: 0.2s;
 	}
 
 	.search {
@@ -38,15 +40,19 @@ const NavStyle = styled.div`
 export const Navigation = () => {
 	const { collapse } = useContext(ToggleContext);
 
+	const location = useLocation();
+
 	return (
 		<NavStyle>
 			<Navbar
 				expand="lg"
 				sticky="top"
-				className={(collapse ? 'closedNav' : 'openNav') + ' fixed-top'}
+				className={(collapse ? 'closedNav' : 'openNav')}
 			>
 				<Container>
-					<div className="mr-auto search">
+					<div
+						className="mr-auto search"
+						style={{ visibility: (location.pathname === '/' ? 'hidden' : 'visible') }}>
 						<Search />
 					</div>
 					<div className='ml-auto'>
