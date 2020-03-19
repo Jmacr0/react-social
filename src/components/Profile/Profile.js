b24ee00522da1903748df091a87482457eb8b2f0
Jmacro <jonmacrohon@gmail.com>
2020-03-20 00:29:33 +1100

    retriever logged in user details and render

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,34 +1,66 @@
-import React, { useContext } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState, useContext, useEffect } from 'react';
+import { Jumbotron, Container, Row, Col, Badge, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { ToggleContext } from '../../utils/ToggleContext';
 import styled from 'styled-components';
+import API from '../../utils/API';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 const ContentRight = styled.div`
 	&.openNav {
 		margin-left: 220px;
-		padding: 0px 10px;
+		padding: 0px;
+		transition-duration: 0.2s;
 	}
 	&.closedNav {
 		margin-left: 50px;
-		padding: 0px 10px;
+		padding: 0px;
+		transition-duration: 0.2s;
 	}
 `
 
 export const Profile = () => {
 	const toggle = useContext(ToggleContext);
+	const [reviewsTotal, setReviewsTotal] = useState();
+	const [username, setUsername] = useState();
+	const [bio, setBio] = useState();
+
+	const loadUser = async () => {
+		const foundUser = await API.user.getCurrentUser();
+		setReviewsTotal(foundUser.reviews.length)
+		setUsername(foundUser.username);
+		setBio(foundUser.bio)
+	}
+
+	useEffect(() => {
+		loadUser();
+	}, [])
 
 	return (
 		<ContentRight className={toggle.collapse ? 'closedNav' : 'openNav'}>
+			<Jumbotron>
+				<Row>
+					<Col>
+						<h2>{username}</h2>
+					</Col>
+					<Col>
+						<Button variant="danger" disabled>
+							Total Reviews: <Badge variant="light">{reviewsTotal}</Badge>
+							<span className="sr-only">unread messages</span>
+						</Button>
+					</Col>
+					<Col>
+						<Link to='/profile/edit'>
+							<FontAwesomeIcon style={{ fontSize: '2em', color: 'white' }} icon={faCog} />
+						</Link>
+					</Col>
+				</Row>
+			</Jumbotron>
 			<Container>
 				<Row>
 					<Col>
-						<h2>Username</h2>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum totam explicabo nostrum aspernatur nobis nulla atque nemo quos, adipisci est eos iure quasi, dignissimos inventore consectetur aperiam sequi perspiciatis quidem?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus quae molestias et quas iste consectetur amet eligendi saepe laudantium voluptate, consequuntur atque eos harum provident dolore at, molestiae impedit! Voluptas!</p>
-						<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum totam explicabo nostrum aspernatur nobis nulla atque nemo quos, adipisci est eos iure quasi, dignissimos inventore consectetur aperiam sequi perspiciatis quidem?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus quae molestias et quas iste consectetur amet eligendi saepe laudantium voluptate, consequuntur atque eos harum provident dolore at, molestiae impedit! Voluptas!</p>
+						<p>{bio}</p>
 					</Col>
 				</Row>
 			</Container>
