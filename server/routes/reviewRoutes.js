db6a856cfd54baa66c6dcc09c88e00b6684417b5
Jmacro <jonmacrohon@gmail.com>
2020-03-23 17:18:43 +1100

    added route for search and by type/category

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -4,6 +4,12 @@ const reviewController = require('../controllers/reviewController');
 router.route('/all')
 	.get(reviewController.findAll)
 
+router.route('/all/:type')
+	.get(reviewController.findAllType)
+
+router.route('/all/search/:search')
+	.get(reviewController.findAllSearch)
+
 router.route('/new')
 	.post(reviewController.saveNew)
 
