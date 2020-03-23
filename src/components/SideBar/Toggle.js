81d7052c24a7d0e37ede27e2336b6130e47dad0c
Jmacro <jonmacrohon@gmail.com>
2020-03-23 18:19:51 +1100

    change toggle to hanburger and cross, removed arrow, and used react media

--- src/components/SideBar/Toggle.js
+++ src/components/SideBar/Toggle.js
@@ -1,46 +1,53 @@
 import React, { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard } from '@fortawesome/free-regular-svg-icons';
-import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import styled from 'styled-components';
 import { ToggleContext } from '../../utils/ToggleContext';
+import Media from 'react-media';
 
 const IconStyle = styled.div`
 	&& {
 		color: white;
 		font-size: 30px;
-		margin-right: 10px;
-		width: 200px;		
-	}	
-
-	.cursor {
+		width: 200px;
 		cursor: pointer;
-	}
-
-	.arrow {
-		color: grey;
-		animation-name: tap;
-		animation-duration: 0.5s;
-		animation-direction: alternate;
-		animation-iteration-count: infinite;
-		animation-timing-function: cubic-bezier(1, 1, 1, 0.2);
-	}
-
-	@keyframes tap {
-		from {margin-left: 0px;}
-		to {margin-left: 10px;}
-	}
+		transition-duration: 0.2s;
+		white-space: nowrap;
+		&:hover {
+			background-color: #303030;
+		}
+	}	
 `
 
 export const Toggle = () => {
 	const { collapse, onClick } = useContext(ToggleContext);
-	const arrow = collapse ? '' : <FontAwesomeIcon icon={faLongArrowAltLeft} className='arrow' />;
 
 	return (
-		<IconStyle>
-			<FontAwesomeIcon onClick={() => onClick(collapse ? false : true)} icon={faAddressCard} className='cursor ml-2' />
-			{arrow}
-		</IconStyle>
+		<Media queries={{ small: { maxWidth: 599 } }}>
+			{matches =>
+				matches.small ? (
+					<IconStyle
+						className='m-0 w-100'
+						onClick={() => onClick(collapse ? false : true)}
+					>
+						<FontAwesomeIcon
+							className='mx-2'
+							icon={collapse ? faBars : faTimes} />
+						{collapse ? '' : <span style={{ fontSize: '1rem', verticalAlign: 'baseline' }}> CLOSE</span>}
+					</IconStyle>
+				) : (
+						<IconStyle
+							className='m-0 w-100'
+							onClick={() => onClick(collapse ? false : true)}
+						>
+							<FontAwesomeIcon
+								className='mx-2'
+								icon={collapse ? faBars : faTimes} />
+							{collapse ? '' : <span style={{ fontSize: '1rem', verticalAlign: 'baseline' }}> CLOSE</span>}
+						</IconStyle>
+					)
+			}
+		</Media>
 	);
 }
 
