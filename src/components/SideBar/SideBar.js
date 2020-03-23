a9d353815277249ac3c841adf187729a63a04998
Jmacro <jonmacrohon@gmail.com>
2020-03-23 18:47:42 +1100

    added react media for small devices

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -5,6 +5,7 @@ import { Menus } from './Menus';
 import { Toggle } from './Toggle';
 import { ToggleContext } from '../../utils/ToggleContext';
 import { LevelBar } from './LevelBar';
+import Media from 'react-media';
 
 const SideNav = styled.div`
 		height: 100%; 
@@ -23,18 +24,44 @@ const SideNav = styled.div`
 		}
 `;
 
+const SideNavResponsive = styled.div`
+		height: 100px; 
+		width: 100%;
+		position: fixed;
+		z-index: 9999;
+		background-color: #222; 
+		overflow-x: hidden;
+		transition: 0.2s;
+
+		&.closed {
+			height: 40px;
+			transition: 0.2s;
+		}
+`
+
 
 export const SideBar = () => {
 	const { collapse } = useContext(ToggleContext);
 
 	return (
 		<>
-			<SideNav className={collapse ? 'closed' : ''}>
-				<Toggle />
-				<DisplayProfile />
-				<LevelBar />
-				<Menus />
-			</SideNav>
+			<Media queries={{ small: { maxWidth: 599 } }}>
+				{matches =>
+					matches.small ? (
+						<SideNavResponsive className={collapse ? 'closed' : ''}>
+							<Toggle />
+						</SideNavResponsive>
+					) : (
+							<SideNav className={collapse ? 'closed' : ''}>
+								<Toggle />
+								<DisplayProfile />
+								<LevelBar />
+								<Menus />
+							</SideNav>
+						)
+				}
+			</Media>
+
 		</>
 	)
 }
