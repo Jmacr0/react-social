c6cd726536121c423fd4785a1e7f8ac34dfff93b
Jmacro <jonmacrohon@gmail.com>
2020-03-23 18:20:51 +1100

    added function that takes user input and sends it to server and retrieves matching reviews

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { CategoryContext } from '../../utils/CategoryContext';
 
-export const Search = () => {
+export const Search = ({ main }) => {
+	const { onSearch } = useContext(CategoryContext);
+	const [redirect, setRedirect] = useState();
+	const [searchTerm, setSearchTerm] = useState();
+
+	const handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			handleSubmit();
+		}
+	}
+
+	const handleSubmit = async () => {
+		setRedirect('/feed')
+		onSearch(searchTerm);
+	}
+
 	return (
 		<div>
-			<input placeholder='Search' className='rounded p-1 my-1' style={{ border: 'none' }} />
+			<input onKeyDown={handleKeyDown} className={main ? 'p-2 mr-1 rounded' : 'm-1 p-1 rounded'} placeholder='Search' value={searchTerm || ''} onChange={(e) => setSearchTerm(e.target.value)} style={{ border: 'none' }} />
+			<FontAwesomeIcon icon={faSearch} onClick={handleSubmit} style={{ cursor: 'pointer', fontSize: (main ? '2rem' : ''), color: 'white' }} />
+			{redirect ? <Redirect to='/feed' /> : ''}
 		</div>
 	)
 }
