c0c538b9237c983287f5df04116372bf5a431c88
Jmacro <jonmacrohon@gmail.com>
2020-03-30 00:31:52 +1100

    consolidate conditional rendering of routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,9 +6,13 @@ import { Category } from './Category/Category';
 import { Profile } from './Profile/Profile';
 import { Route } from 'react-router-dom'
 import { Feed } from './Feed/Feed';
+import { EditReview } from './EditReview/EditReview';
+import { CommentEdit } from './Comment/CommentEdit';
+import { Favourites } from './Favourites/Favourites';
 import styled from 'styled-components';
 import { ToggleContext } from '../utils/ToggleContext';
 import Media from 'react-media';
+
 
 const ContentRight = styled.div`
 	&.openNav {
@@ -20,36 +24,40 @@ const ContentRight = styled.div`
 		transition-duration: 0.2s;
 	}
 	&.smallDeviceOpen {
-		margin-top: 100px;
+		margin-top: 90px;
 		transition-duration: 0.2s;
 	}
 	&.smallDeviceClosed {
-		margin-top: 40px;
+		margin-top: 45px;
 		transition-duration: 0.2s;
 	}
 `
 export const Main = () => {
 	const { collapse } = useContext(ToggleContext);
+	const routes = (
+		<>
+			<Route exact path='/profile/edit' component={ProfileEdit} />
+			<Route exact path='/profile' component={Profile} />
+			<Route exact path='/review/one/:id' component={Review} />
+			<Route exact path='/review/one/edit/:id' component={EditReview} />
+			<Route exact path='/review/new' component={CreateReview} />
+			<Route exact path='/comment/one/edit/:id' component={CommentEdit} />
+			<Route exact path='/favourites' component={Favourites} />
+			<Route exact path='/feed/:category' component={Feed} />
+			<Route exact path='/' component={Category} />
+		</>
+	)
+
 	return (
 		<Media queries={{ small: { maxWidth: 599 } }}>
 			{matches =>
 				matches.small ? (
 					<ContentRight className={collapse ? 'smallDeviceClosed' : 'smallDeviceOpen'}>
-						<Route exact path='/profile/edit' component={ProfileEdit} />
-						<Route exact path='/profile' component={Profile} />
-						<Route exact path='/review/one/:id' component={Review} />
-						<Route exact path='/review/new' component={CreateReview} />
-						<Route exact path='/feed' component={Feed} />
-						<Route exact path='/' component={Category} />
+						{routes}
 					</ContentRight>
 				) : (
 						<ContentRight className={collapse ? 'closedNav' : 'openNav'}>
-							<Route exact path='/profile/edit' component={ProfileEdit} />
-							<Route exact path='/profile' component={Profile} />
-							<Route exact path='/review/one/:id' component={Review} />
-							<Route exact path='/review/new' component={CreateReview} />
-							<Route exact path='/feed' component={Feed} />
-							<Route exact path='/' component={Category} />
+							{routes}
 						</ContentRight>
 					)
 			}
