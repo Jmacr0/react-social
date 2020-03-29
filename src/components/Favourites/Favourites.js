60c28058d9e1dc9c1a642a3d657d42651488a63e
Jmacro <jonmacrohon@gmail.com>
2020-03-30 00:38:33 +1100

    init favourites page

---
+++ src/components/Favourites/Favourites.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import API from '../../utils/API';
+import { FeedReview } from '../Feed/FeedReview';
+
+export const Favourites = () => {
+	const [favouriteReviews, setFavouriteReviews] = useState([]);
+	// const descFavourites = React.useMemo(() => {
+	// 	return favourites.slice(0).reverse();
+	// }, [favourites]);
+
+
+	// console.log()
+	// const [descReviews, setDescReviews] = useState([]);
+	// const reverseOrder = () => {
+	// 	const reverseReviews = reviews.reverse();
+	// 	setDescReviews(reverseReviews);
+	// }
+
+	const loadFavourites = () => {
+		API.favourite.getFavouriteReviews()
+			.then(result => {
+				console.log(result);
+				setFavouriteReviews(result);
+			})
+	}
+
+	useEffect(() => {
+		loadFavourites();
+	}, [])
+
+	return (
+		<>
+			<Container style={{ backgroundColor: '#00346e' }} fluid>
+				<div className='text-center'>
+					<h2>My Favourites</h2>
+				</div>
+				{favouriteReviews.map((favourite, index) =>
+					<FeedReview review={favourite.review} key={index} />
+				)}
+			</Container>
+		</>
+	)
+}
+
