60b8f100e73e7cf176ed34a68a371856e59ae627
Jmacro <jonmacrohon@gmail.com>
2020-03-30 00:40:00 +1100

    use load reviews from user context and pass as params to feedreview. useParams to filter category

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,44 +2,75 @@ import React, { useContext, useEffect, useState, } from 'react';
 import API from '../../utils/API';
 import { FeedReview } from './FeedReview';
 import { Container } from 'react-bootstrap';
-import { CategoryContext } from '../../utils/CategoryContext';
+import { SearchContext } from '../../utils/SearchContext';
+import { useParams } from 'react-router-dom';
+import { UserContext } from '../../utils/UserContext';
 
 export const Feed = () => {
-	const { selection, search } = useContext(CategoryContext);
+	const { search } = useContext(SearchContext);
 	const [reviews, setReviews] = useState([]);
 
+	const { category } = useParams();
+	const { id } = useContext(UserContext);
+
+
 	const loadReviews = () => {
-		if (selection === 'All' || selection === 'New') {
+		if (category === 'All') {
 			API.review.getReviewsAll()
 				.then(reviews => {
-					setReviews(reviews)
+					console.log('all: ', reviews)
+					setReviews(reviews);
 					return;
 				})
 		} else {
-			API.review.getReviewsOneType(selection)
+			API.review.getReviewsOneType(category)
 				.then(reviews => {
-					setReviews(reviews)
+					console.log('certain type: ', reviews)
+					setReviews(reviews);
 					return;
 				})
 		}
 		if (search) {
 			API.review.getReviewsSearch(search)
 				.then(reviews => {
-					setReviews(reviews)
+					setReviews(reviews);
 					return;
 				})
 		}
 	}
 
+	// const handleFeedReviewFavouriteChange = (review_id, isFavourite) => {
+	// 	const newReviews = [];
+	// 	for (const review of reviews) {
+	// 		const newReview = { ...review };
+	// 		if (newReview._id === review_id) {
+	// 			if (isFavourite) {
+	// 				newReview.favourites.push({
+	// 					user: id,
+	// 					review: review_id,
+	// 				})
+	// 			} else {
+	// 				newReview.favourites = newReview.favourites.filter((favourite) => favourite.user_id !== id);
+	// 			}
+	// 		}
+	// 		newReviews.push(newReview);
+	// 	}
+	// 	this.setReviews(newReviews);
+	// };
+
 	useEffect(() => {
 		loadReviews();
-	}, [search])
+	}, [search, category]);
 
 	return (
 		<>
-			<Container fluid>
+			<Container style={{ backgroundColor: '#00346e' }} fluid>
 				{reviews.map((review, index) =>
-					<FeedReview review={review} key={index} />
+					<FeedReview
+						review={review}
+						loadReviews={loadReviews}
+						// handleFavouriteChange={handleFeedReviewFavouriteChange}
+						key={index} />
 				)}
 			</Container>
 		</>
