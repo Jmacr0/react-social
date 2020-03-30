e19559f4c513f9ce58fb7883882105965d380a03
Jmacro <jonmacrohon@gmail.com>
2020-03-30 23:10:17 +1100

    heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes");
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
