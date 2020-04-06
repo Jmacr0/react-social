8a61266a4a65374a9028eadba3e7d6270aa73ca3
Jmacr0 <jonmacrohon@gmail.com>
2020-04-06 20:31:01 +1000

    add modal for logging out

--- client/src/components/SideBar/Menus.js
+++ client/src/components/SideBar/Menus.js
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { MenuLink } from './MenuLink';
-import { faCog, faUser, faHeart, faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faSignOutAlt, faUser, faHeart, faNewspaper } from '@fortawesome/free-solid-svg-icons';
 import API from '../../utils/API';
+import { ModalLogout } from '../Main/Modal/ModalLogout';
 
 export const Menus = () => {
-	const [redirect, setRedirect] = useState();
 
 	const handleLogout = async () => {
 		const response = await API.user.logoutUser();
 		localStorage.setItem('user', '');
-		setRedirect(response);
+		history.replace('/sign-up');
 	}
+
+	// Modal management
+	const history = useHistory();
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
 	return (
 		<>
 			<MenuLink iconStyle={faUser} value={'PROFILE'} link={'/profile'} />
 			<MenuLink iconStyle={faNewspaper} value={'NEW REVIEW'} link={'/review/new'} />
 			<MenuLink iconStyle={faHeart} value={'MY FAVOURITES'} link={'/favourites'} />
-			<MenuLink iconStyle={faCog} value={'LOGOUT'} onLogout={handleLogout} />
-			{redirect ? <Redirect to={redirect} /> : ''}
+			<MenuLink iconStyle={faSignOutAlt} value={'LOGOUT'} onShow={handleShow} />
+			<ModalLogout
+				title='Confirm Logout'
+				show={show}
+				onClose={handleClose}
+				onLogout={handleLogout}
+			/>
 		</>
 	)
 }
